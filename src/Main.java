536dffdba9462f83a4290867fe0f0f01de89407f
youtehyuk <youtehyuk0126@gmail.com>
2024-10-18 05:13:32 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+//        User user = new User("유태혁", 21, "운동"); // 클래스 생성
+        User user = new User();
+//        user.setName("유태혁");
+
+        System.out.println(user.getName());
+//        System.out.println(user.age);
+//        System.out.println(user.hobby);
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,41 @@
+public class User {
+
+    String name;
+    int age;
+    String hobby;
+
+//    public User () {}
+//
+//    public User (String name, int age, String hobby) {
+//        this.name = name;
+//        this.age = age;
+//        this.hobby = hobby;
+//
+//    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+}
